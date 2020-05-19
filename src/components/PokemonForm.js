9caf6dc40e1a561bab6d732efda2f386894fd5a3
AidanMcB <aidankmcbride@gmail.com>
2020-05-19 13:37:05 -0500

    Done.

--- src/components/PokemonForm.js
+++ src/components/PokemonForm.js
@@ -13,11 +13,20 @@ class PokemonForm extends React.Component {
     }
   }
 
+  //set up a newPokemon to send to the page
+  //call handleSubmit pokmeon at the bottom
+  createANewPokemon = (event) => {
+    this.setState({
+      name: event.target.name.value,
+      hp: event.target.hp.value
+    }, () => this.props.handleSubmitPokemon(this.state))
+  }
+
   render() {
     return (
       <div>
         <h3>Add a Pokemon!</h3>
-        <Form onSubmit={() => {console.log("submitting form...")}}>
+        <Form onSubmit={this.createANewPokemon}>
           <Form.Group widths="equal">
             <Form.Input fluid label="Name" placeholder="Name" name="name" />
             <Form.Input fluid label="hp" placeholder="hp" name="hp" />
