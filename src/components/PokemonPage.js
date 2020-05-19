2b22f041b5d14360cbf5049f99193f4a9b1ee67a
ezrablack <timothydanielmorales@gmail.com>
2020-05-19 09:05:05 -0500

    Done.

--- src/components/PokemonPage.js
+++ src/components/PokemonPage.js
@@ -5,17 +5,63 @@ import Search from './Search'
 import { Container } from 'semantic-ui-react'
 
 class PokemonPage extends React.Component {
+
+  state = {
+    pokeIndex: [],
+    filteredPoke: []
+  }
+
+  componentDidMount(){
+    this.refreshPokemon()
+  }
+
+  pokeFilter = (e) => {
+    //query to lowercase
+    let currentList = this.state.pokeIndex
+    let filterList = []
+    let search = e.target.value
+    if (search !== ''){
+      filterList = currentList.filter(pokemon => {
+        const poke = pokemon.name.toLowerCase()
+        const query = search.toLowerCase()
+        return poke.includes(query)
+      })
+    }else{
+        filterList = currentList
+    }
+    this.setState({
+      filteredPoke: filterList
+    })
+  }
+
+  refreshPokemon = () =>{
+    fetch('http://localhost:3000/pokemon')
+      .then(res=>res.json())
+      .then((pokemon) => {
+        this.setState({
+          pokeIndex: pokemon,
+          filteredPoke: pokemon
+        })
+      })
+  }
+
+
   render() {
     return (
       <Container>
         <h1>Pokemon Searcher</h1>
         <br />
-        <PokemonForm />
+        <PokemonForm refreshPokemon = {this.refreshPokemon} />
         <br />
-        <Search />
+        <Search 
+        pokeFilter ={this.pokeFilter}
+        />
         <br />
-        <PokemonCollection />
+        <PokemonCollection 
+        pokeIndex = {this.state.filteredPoke}
+        />
       </Container>
+    
     )
   }
 }
