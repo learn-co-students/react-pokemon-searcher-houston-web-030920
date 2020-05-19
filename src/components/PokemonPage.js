98c65ec681e74c8d5112ac8591dde023f5e59b08
bryantomoregie <bryantomoregie@yahoo.com>
2020-05-19 07:45:53 -0500

    Done.

--- src/components/PokemonPage.js
+++ src/components/PokemonPage.js
@@ -5,16 +5,80 @@ import Search from './Search'
 import { Container } from 'semantic-ui-react'
 
 class PokemonPage extends React.Component {
+
+  state =  {
+    pokemons: [],
+    filterPokemons: []
+  }
+
+
+  filterPokemon = (letter) => {
+    console.log(letter)
+    this.setState({
+      filterPokemons: this.state.filterPokemons.filter((pokemon) => {
+        return pokemon.name.startsWith(letter)
+    })
+    })
+  }
+
+  newPokemon = (e) => {
+    fetch('http://localhost:3000/pokemon',{
+      method: "POST",
+      headers: {"Content-Type" : "application/json"}, 
+      body: JSON.stringify
+      ({
+          name: e.target.name.value,
+          stats: [
+            {
+            value: e.target.hp.value,
+            name: "hp"
+          }],
+          sprites: {
+            front: e.target.frontUrl.value,
+            back: e.target.backUrl.value
+          }
+      })
+    })
+    .then(res => res.json())
+    .then(pokemon => {
+      this.setState({
+        pokemons: [...this.state.pokemons, pokemon]
+      })
+    })
+  }
+
+
+    componentDidMount() {
+    fetch('http://localhost:3000/pokemon')
+    .then(res => res.json())
+    .then(pokemons => {
+      this.setState(
+        {pokemons: pokemons,
+        filterPokemons: pokemons
+        }
+      )
+    })
+  }
+
+
+
+
   render() {
     return (
       <Container>
         <h1>Pokemon Searcher</h1>
         <br />
-        <PokemonForm />
+        <PokemonForm
+        newPokemon={this.newPokemon}
+        />
         <br />
-        <Search />
+        <Search 
+        filterPokemon={this.filterPokemon}
+        />
         <br />
-        <PokemonCollection />
+        <PokemonCollection
+        pokemons={this.state.filterPokemons}
+        />
       </Container>
     )
   }
